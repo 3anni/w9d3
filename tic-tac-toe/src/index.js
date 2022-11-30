8fd07901365ef89841e56af73c268fe2e1d4a7ef
Will Bannister <bannister.will@gmail.com>
2022-11-30 15:09:34 -0800

    -m

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,6 +1,19 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require ('./ttt-view.js');// require appropriate file
+const Game = require ('../ttt_node/game.js')// require appropriate file
+
+
+console.log('in index.js');
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+
+  console.log('in DOMContentLoaded block');
+  let game = new Game();
+  let el = document.querySelector('.ttt'); // this is going to be the figure 'ttt'
+  let view = new View(game, el);
+
+
+
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
--- tic-tac-toe/src/ttt-view.js
+++ tic-tac-toe/src/ttt-view.js
@@ -1,13 +1,37 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
-  
-  bindEvents() {}
+  setupBoard() {
+    let list = this.el.createElement('ul');
+    list.classList.add("grid_style");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let pos = [i,j];
+        let li = document.createElement('li');
+        li.classList.add("li-style");
+        li['data-pos'] = pos;
+        list.appendChild(li); // append also works
+      }
+    }
+    this.el.appendChild(list);
+    console.log('board loaded');
+  }
 
-  handleClick(e) {}
+  bindEvents() {
 
-  makeMove(square) {}
+  }
+
+  handleClick(e) {
+    //this.li()
+  }
+
+  makeMove(square) {
+
+  }
 
 }
 
